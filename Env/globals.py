6b84d1be61427d24e3eb7ef74075538464fc0b78
Ganesh kumar T K <ganeshkumartk@outlook.com>
2019-07-21 10:59:19 +0530

    ENV 👍✔

---
+++ Env/globals.py
@@ -0,0 +1,99 @@
+import numpy as np
+import math
+
+N_PLAYER = 5
+MAP_WIDTH = 14400
+MAP_HEIGHT = 9600
+GOALWIDTH = 3000
+W_BINS = 64
+H_BINS = 48
+FORCE_MAX = 100
+FORCE_MIN = 20
+FORCE_FD = 4
+ROOT_STATE_SIZE = 1 - 1 + (2 + 2) + (2 * N_PLAYER) + (2 * N_PLAYER)
+STATE_SIZE = ROOT_STATE_SIZE * (ROOT_STATE_SIZE + 1)
+MOVE_ACTION_SIZE = W_BINS * H_BINS
+SHOOT_ACTION_SIZE = MOVE_ACTION_SIZE * FORCE_FD
+SHOOT_RANGE = 200
+MOVE_ACTION = 1
+SHOOT_ACTION = 2
+
+SEVER_SIZE = 4 + (2 + 2) + (3 * N_PLAYER) + (3 * N_PLAYER)
+BOT_SIZE = 4 * N_PLAYER
+
+GAMMA = 0.9
+MODEL_SAVE_PATH = "check_points/best_model.ckpt"
+REPLAY_SAVE_PATH = "matches_data/replay"
+HALF_1 = 1
+HALF_2 = 2
+
+
+class Position:
+    def __init__(self, _x = 0, _y = 0):
+        self.x = _x
+        self.y = _y
+    
+class Object:
+    def __init__(self):
+        self.ID = 0
+        self.m_action = 0
+        self.m_force = 0
+        self.m_pos = Position()
+        self.m_targetPos = Position()
+        self.m_moveSpeed = Position()
+        
+def distance(A, B):
+    return math.sqrt((A.m_pos.x - B.m_pos.x)**2 + (A.m_pos.y - B.m_pos.y)**2)
+        
+def to_pos_x_y(q_id):
+    x = int(q_id / H_BINS)
+    y = q_id % H_BINS
+    x = int(x * (float(MAP_WIDTH) / (W_BINS - 1)))
+    y = int(y * (float(MAP_HEIGHT) / (H_BINS - 1)))
+    return x, y
+    
+def to_pos_x_y_f(q_id):
+    x = q_id / (H_BINS * FORCE_FD)
+    r = q_id % (H_BINS * FORCE_FD)
+    y = r / FORCE_FD
+    f = r % FORCE_FD
+    x = int(x * (float(MAP_WIDTH) / (W_BINS - 1)))
+    y = int(y * (float(MAP_HEIGHT) / (H_BINS - 1)))
+    f = int(f * (float(FORCE_MAX - FORCE_MIN) / (FORCE_FD - 1))) + FORCE_MIN
+    return x, y, f
+
+def x_y_to_id(x, y):
+    r0_q_id = int(1.0 * x * (W_BINS - 1) / MAP_WIDTH)
+    r1_q_id = int(1.0 * y * (H_BINS - 1) / MAP_HEIGHT)
+    return r0_q_id * H_BINS + r1_q_id
+    
+def x_y_f_to_id(x, y, f):
+    r0_q_id = int(1.0 * x * (W_BINS - 1) / MAP_WIDTH)
+    r1_q_id = int(1.0 * y * (H_BINS - 1) / MAP_HEIGHT)
+    r2_q_id = int(1.0 * (f - FORCE_MIN) * (FORCE_FD - 1) / (FORCE_MAX - FORCE_MIN))
+    return r0_q_id * H_BINS * FORCE_FD + r1_q_id * FORCE_FD + r2_q_id
+
+def input_to_states(gameTurn, oBall, Player_A, Player_B, Bmap):
+    state = np.array([0.0 for _ in range(STATE_SIZE+1)])
+    state[0] = gameTurn
+    state[1] = oBall.m_pos.x
+    state[2] = oBall.m_pos.y
+    state[3] = oBall.m_moveSpeed.x
+    state[4] = oBall.m_moveSpeed.y
+    
+    for i in range(N_PLAYER):
+        state[5+i] = Player_A[i].m_pos.x
+        state[10+i] = Player_A[i].m_pos.y
+        state[15+i] = Player_B[Bmap[i]].m_pos.x
+        state[20+i] = Player_B[Bmap[i]].m_pos.y
+    return state[1:]
+    
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
---
+++ Env/layer_output_test.py
@@ -0,0 +1,61 @@
+import os, subprocess
+import tensorflow as tf
+import numpy as np
+import globals, model, trainBot
+from features import states_to_complex_states
+
+if __name__ == '__main__':
+    if not os.path.exists("unit_test_outputs/"):
+        os.makedirs("unit_test_outputs/")
+
+    if 'session' in locals() and session is not None:
+        session.close()
+    with tf.Session() as sess:
+        team_group = model.Group()
+        sess.run(tf.global_variables_initializer())
+        print("Loading Model...")
+        t_vars = tf.trainable_variables()
+        vars_list = [var for var in t_vars]
+        saver = tf.train.Saver(var_list=vars_list)
+        saver.restore(sess, globals.MODEL_SAVE_PATH)
+        model.save_running_model_matrices(sess)
+        print("Done!")
+    
+        trainer = trainBot.Trainer(team_group, 0, 0, sess)
+        
+        with open("layer_output_test_input.txt", "r") as in_file:
+            server_content = in_file.read()
+        
+        print(server_content)
+        
+        si = trainBot.ServerInfo()
+        si.parse_server_content(server_content)
+        complex_state = states_to_complex_states(globals.input_to_states(si.gameTurn, si.oBall, si.Player_A, si.Player_B, si.features.Bmap))
+        # print(complex_state)
+
+        
+        outputs = trainer.perform_layer_output_test(np.expand_dims(complex_state, axis=0), globals.N_PLAYER)
+        
+        thd = subprocess.Popen(['cmd', '/c', r'BotDemo.exe', server_content])
+        thd.wait()
+        
+        C_outputs = [None for _ in range(globals.N_PLAYER)]
+        
+        
+        for i in range(globals.N_PLAYER):
+            print(f"Player {i}-th")
+            file_name = f"unit_test_outputs/test_layer_p{i}.txt"
+            C_outputs[i] = np.loadtxt(file_name)
+            C_matrix = np.array(C_outputs[i]).flatten()
+            P_matrix = np.array(outputs[i]).flatten()
+            print(globals.bcolors.OKBLUE+"Python:"+globals.bcolors.ENDC)
+            print(outputs[i][:10])
+            print(globals.bcolors.OKGREEN+"C++:"+globals.bcolors.ENDC)
+            print(C_outputs[i][:10])
+            print(globals.bcolors.WARNING+"Unmatched entries: "+globals.bcolors.ENDC)
+            print(globals.bcolors.FAIL+"Python:"+globals.bcolors.ENDC)
+            print((P_matrix[:10])[np.abs(C_matrix-P_matrix)[:10] > 1e-7])
+            print(globals.bcolors.FAIL+"C++:"+globals.bcolors.ENDC)
+            print((C_matrix[:10])[np.abs(C_matrix-P_matrix)[:10] > 1e-7])
+            print("------------------------")
+        
